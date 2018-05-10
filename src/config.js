54eae765fcac391b878cea08d481139066d1b388
DongHyun Kim <codegizer.kim@gmail.com>
2018-05-11 03:21:58 +0900

    config.js 콘솔확이 및 package.json의 react-scripts 버전 수정

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 var config = {
- apiKey: process.env.REACT_APP_FIREBASE_KEY,
+ apiKey: process.env.REACT_APP_APIKEY,
+ authDomain: process.env.REACT_APP_AUTH_DOMAIN,
  databaseURL: process.env.REACT_APP_DB_URL,
+ projectId: process.env.REACT_APP_PRJ_ID,
  storageBucket: process.env.REACT_APP_STRG_BKT,
  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
  embedlyKey : process.env.REACT_APP_EMBEDLY_KEY
