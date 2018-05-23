11b6ac7f4d5f40eafa36f8390829eec1f0267c66
DongHyun Kim <codegizer.kim@gmail.com>
2018-05-24 00:16:07 +0900

    환경변수 수정

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 var config = {
- apiKey: process.env.REACT_APP_APIKEY,
+ apiKey: process.env.REACT_APP_FIREBASE_KEY,
  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
  databaseURL: process.env.REACT_APP_DB_URL,
  projectId: process.env.REACT_APP_PRJ_ID,
